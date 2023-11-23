f65a98bbd5a451d37342a3e775ab4f4c6b1adf19
ss <1029515726@qq.com>
2023-11-23 17:26:07 +0800

    first commit

---
+++ config/defaultSettings.ts
@@ -0,0 +1,22 @@
+import { ProLayoutProps } from '@ant-design/pro-components';
+
+/**
+ * @name
+ */
+const Settings: ProLayoutProps & {
+  pwa?: boolean;
+  logo?: string;
+} = {
+  navTheme: "light",
+  colorPrimary: "#722ED1",
+  layout: "mix",
+  contentWidth: "Fluid",
+  fixedHeader: false,
+  fixSiderbar: true,
+  pwa: true,
+  logo: "https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg",
+  token: {}
+};
+
+export default Settings;
+
