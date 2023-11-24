83c733942d3790e3a0b5f66471554d054fe87b75
ss <1029515726@qq.com>
2023-11-24 08:41:31 +0800

    模板样式修改

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -7,15 +7,16 @@ const Settings: ProLayoutProps & {
   pwa?: boolean;
   logo?: string;
 } = {
-  navTheme: "light",
-  colorPrimary: "#722ED1",
-  layout: "mix",
-  contentWidth: "Fluid",
+  navTheme: 'light',
+  colorPrimary: '#722ED1',
+  layout: 'mix',
+  contentWidth: 'Fluid',
   fixedHeader: false,
   fixSiderbar: true,
   pwa: true,
-  logo: "https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg",
-  token: {}
+  logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+  token: {},
+  splitMenus: false,
 };
 
 export default Settings;
